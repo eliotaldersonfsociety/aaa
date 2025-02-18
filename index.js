a7ca3d7f947e587baff88252bf3536f2f20e832a
eliotaldersonfsociety <aldersonelliot803@gmail.com>
2025-02-18 18:46:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,57 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const authRouter = require('./routes/auth');
+const postRouter = require('./routes/post');
+const userRouter = require('./routes/user');
+const actualizarRouter = require('./routes/actualizar');
+const { createUserTable } = require('./models/user');
+const { createPurchasesTable } = require('./models/purchase');
+
+dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
-// Configurar Express
-app.use(express.json());
+// Habilitar CORS
+app.use(cors({
+  origin: "*", // Permitir solicitudes desde el frontend
+  credentials: true, // Permitir cookies y autenticación con credenciales
+}));
 
-app.get('/api/login', async (req, res) => {
-  // Usamos una plantilla de literal (backticks) para el HTML
-  const htmlResponse = `
-  <html>
-    <head>
-      <title>Asi es mano</title>
-    </head>
-    <body>
-      <h1>Claro que si papi</h1>
-    </body>
-  </html>
-  `;
-  res.send(htmlResponse);
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'API is working!' });
 });
 
-app.listen(port, () => {
-  console.log(`Puerto corriendo aqui: http://localhost:${port}`);
+app.get('/test-db', async (req, res) => {
+  try {
+    const users = await client.execute({
+      sql: 'SELECT * FROM users',
+      args: [],
+    });
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: 'Error connecting to the database' });
+  }
 });
+
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/posts', postRouter);
+app.use('/api/v1/user', userRouter);
+app.use('/api/v1/actualizar', actualizarRouter);
+
+async function startServer() {
+  await createUserTable();
+  console.log('User table created or already exists');
+  await createPurchasesTable();
+  console.log('Purchases table created or already exists');
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+startServer();
